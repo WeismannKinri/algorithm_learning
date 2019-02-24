5b1abba26207f240fb049ca1b6045e0db16a98e0
kincolle <kincolle@gmail.com>
2019-02-24 22:21:01 +0800

    add recurtion code

---
+++ src/java/leetcode/explore/recurtion/ReverseString.java
@@ -0,0 +1,19 @@
+package leetcode.explore.recurtion;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        helper(0, s.length - 1, s);
+    }
+
+    private void helper(int start, int end, char [] s) {
+        if (start >= end) {
+            return;
+        }
+
+        char tmp = s[start];
+        s[start] = s[end];
+        s[end] = tmp;
+
+        helper(start + 1, end - 1, s);
+    }
+}
