d1284b2e722fe200656c7803a8d438e89fcf8b24
chengjin <chengjin@Ctrip.com>
2019-08-08 17:08:30 +0800

    add code

--- src/java/leetcode/explore/binary/tree/InorderTraversal.java
+++ src/java/leetcode/explore/binary/tree/InorderTraversal.java
@@ -1,8 +1,6 @@
 package leetcode.explore.binary.tree;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class InorderTraversal {
 
@@ -49,7 +47,6 @@ public class InorderTraversal {
                 }
             }
         }
-
         return list;
     }
 
@@ -76,10 +73,91 @@ public class InorderTraversal {
     }
 
 
+    public String reverseWords1(String s) {
+        int storeIndex = 0, n = s.length();
+        StringBuilder sb = new StringBuilder(s).reverse();
+        for (int i = 0; i < n; ++i) {
+            if (sb.charAt(i) != ' ') {
+                if (storeIndex != 0) sb.setCharAt(storeIndex++, ' ');
+                int j = i;
+                while (j < n && sb.charAt(j) != ' ') sb.setCharAt(storeIndex++, sb.charAt(j++));
+                String t = new StringBuilder(sb.substring(storeIndex - (j - i), storeIndex)).reverse().toString();
+                sb.replace(storeIndex - (j - i), storeIndex, t);
+                i = j;
+            }
+        }
+        sb.setLength(storeIndex);
+        return sb.toString();
+    }
+
+
+    public void moveZeroes(int[] nums) {
+        if (nums==null)
+            return;
+        int slow=0;
+        for (int i = 0; i< nums.length;i++){
+            if(nums[i]==0){
+                nums[slow++] = nums[i];
+            }
+        }
+        for(int i = slow; i<nums.length;i++){
+            nums[i]=0;
+        }
+    }
+
+
+    public static ListNode detectCycle(ListNode head) {
+        if(head==null||head.next==null)return null;
+        if(head.next==head)return head;
+        ListNode l=head.next;
+        head.next=head;
+        return detectCycle(l);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(0^0);
+    }
+
+//    public static void main(String[] args) {
+//        ListNode l1 = new ListNode(3);
+//        ListNode l2 = new ListNode(2);
+//        ListNode l3 = new ListNode(0);
+//        ListNode l4 = new ListNode(-4);
+//
+//        l1.next=l2;
+//        l2.next=l3;
+//        l3.next=l4;
+//        l4.next=l2;
+//        System.out.println(detectCycle(l1).val);
+//    }
+
     class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
         TreeNode(int x) { val = x; }
+    }
+
+    public int reverseBits(int n) {
+        int result = 0;
+        for(int i = 0; i< 32 ;i++){
+            int temp = n & 1;
+            n = n >> 1;
+            result = (result<<1)|temp;
+        }
+        return result;
+    }
+
+
+
+}
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
     }
 }
